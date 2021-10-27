4521593904fcd958b5ef2b8b362f606858c8d2eb
robbridges <robbridges@users.noreply.github.com>
2021-10-27 11:39:58 -0700

    Letcode max subarray work

---
+++ maxSubArray.js
@@ -0,0 +1,10 @@
+function maxSubArray(A) {
+  let prev = 0;
+  let max = -Number.MAX_VALUE;
+
+  for (var i = 0; i < A.length; i++) {
+    prev = Math.max(prev + A[i], A[i]);
+    max = Math.max(max, prev);
+  }
+  return max;
+}
