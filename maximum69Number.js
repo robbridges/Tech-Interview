7617b2befdeb50f87b8cda2569a2f8a585893a6a
robbridges <robbridges@users.noreply.github.com>
2021-11-01 23:09:48 -0700

    Small adjustment to recent leetcode

--- maximum69Number.js
+++ maximum69Number.js
@@ -1,5 +1,5 @@
 var maximum69Number  = function(num) {
-    return Number(num.toString().replace('6', '9'));
+    return parseInt(num.toString().replace('6', '9'));
 };
 
 console.log(maximum69Number(9669));
