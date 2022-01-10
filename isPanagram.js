ea6c11d05c0198e21aa3f6d3568b2a89d859d544
robbridges <robbridges@users.noreply.github.com>
2022-01-10 14:14:54 -0800

    bit of work for interview tomorrow

--- isPanagram.js
+++ isPanagram.js
@@ -2,6 +2,7 @@
  * @param {string} sentence
  * @return {boolean}
  */
+// doing a bit of a refresher before interview tomorrow, this is simple enough, a set only contains unique values. So if the value of the set is 26, it contains the letter of every alphabet.
  var checkIfPangram = function(sentence) {
   const set = new Set();
   for ( let char of sentence) {
